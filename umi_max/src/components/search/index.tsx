d7d0ad874aaa03856154ffe6a0d2b58c94039cc4
Botrance <1932221806@qq.com>
2023-07-17 17:55:42 +0800

    为添加按钮增加modal

--- umi_max/src/components/search/index.tsx
+++ umi_max/src/components/search/index.tsx
@@ -1,30 +1,77 @@
 import { AddMenuProps, ItemType } from '@/global/define';
 import { PlusOutlined, SearchOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
-import './index.less';
+import { Input, Modal } from 'antd';
 import { useState } from 'react';
+import './index.less';
 
-const items: ItemType[] = [
-  {
-    key: '1',
-    label: <>加好友/群</>,
-  },
-  {
-    key: '2',
-    label: <>创建群聊</>,
-  },
-];
+const FriendLabel: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div onClick={showModal}>加好友/群</div>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        maskClosable={false}
+      >
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
+
+const RoomLabel: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <div onClick={showModal}>创建群聊</div>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        maskClosable={false}
+      >
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+};
 
 const AddMenu: React.FC<AddMenuProps> = ({ items }) => {
   return (
     <div className="add-menu flex-center abs-box">
       {items ? (
         items.map((item) => (
-          <div
-            key={item!.key}
-            className="add-menu-item"
-            onClick={item!.onClick}
-          >
+          <div key={item!.key} className="add-menu-item">
             {item!.label}
           </div>
         ))
@@ -34,6 +81,17 @@ const AddMenu: React.FC<AddMenuProps> = ({ items }) => {
     </div>
   );
 };
+
+const items: ItemType[] = [
+  {
+    key: '1',
+    label: <FriendLabel />,
+  },
+  {
+    key: '2',
+    label: <RoomLabel />,
+  },
+];
 
 export const SearchWithAdd: React.FC = () => {
   const [isAddMenuOpen, setIsAddMenuOpen] = useState(false);
